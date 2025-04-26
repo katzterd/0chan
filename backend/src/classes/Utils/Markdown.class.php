685196f0fb5b50eed364da00b0e80f287b01ac84
katzterd <194586003+katzterd@users.noreply.github.com>
2025-04-27 01:02:31 +0300

    stole devarped markup

--- backend/src/classes/Utils/Markdown.class.php
+++ backend/src/classes/Utils/Markdown.class.php
@@ -21,10 +21,9 @@ class Markdown {
                 'replace'   => '<code>$1</code>',
             ],
             [
-                // old @((https?|ftp)://(\w[\w\-\.]+\w)([:\d]+)?(/(?:[\w\+\?\(\)\#\[\]\-/=_%&\.\,;~]+[\w/])?)?)@
                 'type'      => 'link',
                 'regexp'    => '!(?xi)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`\!()\[\]{};:\'".,<>?«»“”‘’]))!i',
-                'replace'   => '<a href="$1" target="_blank">$1</a>',
+                'replace'   => '<a href="$1" target="_blank" rel="nofollow noopener noreferrer">$1</a>',
             ],
             [
                 'type'      => 'postref',
@@ -52,14 +51,26 @@ class Markdown {
             );
         }
 
-        $message = preg_replace(   '/(^|\s)--($|\s)/', '$1&mdash;$2', $message);
-        $message = preg_replace('/^&gt;\s*(.+)$/m',   '<blockquote>&gt; $1</blockquote>', $message);
+        // типографизация
+        $message = preg_replace('/(^|\s|\*|\^|\%|\~)--($|\s|\*|\^|\%|\~)/', '$1&mdash;$2', $message);
+        $message = preg_replace('/(^|\s|\*|\^|\%|\~)-($|\s|\*|\^|\%|\~)/', '$1&mdash;$2', $message);
+        $message = preg_replace('/(^|\s|\*|\^|\%|\~)&quot;(.+?)&quot;($|\s|\pP|\*|\^|\%|\~)/', '$1&laquo;$2&raquo;$3', $message);
+        // разметка цитат
+        $message = preg_replace('/&gt;&gt;\s*(.+?)\s*&lt;&lt;/m', '<blockquote>&quot;$1&quot;</blockquote>', $message);
+        $message = preg_replace('/^&gt;\s*(.+)$/m', '<blockquote>&gt; $1</blockquote>', $message);
+        $message = preg_replace('/^&lt;\s*(.+)$/m', '<blockquote class="revquo">&lt; $1</blockquote>', $message);
+        // остальная разметка
+	$message = preg_replace('/\(\(\(([^\(\)]+)\)\)\)/', '<span class="them">((($1)))</span>', $message);
         $message = preg_replace('/%%([\s\S]+?)%%/', '<mark>$1</mark>', $message);
         $message = preg_replace('/\*\*([^\*]+)\*\*/', '<b>$1</b>', $message);
-        $message = preg_replace(  '/\*([^\*]+)\*/',   '<i>$1</i>', $message);
+        $message = preg_replace('/\*([^\*]+)\*/',   '<i>$1</i>', $message);
+        $message = preg_replace('/\^\^([^\^]+)\^\^/', '<u>$1</u>', $message);
+        $message = preg_replace('/\~\~([^\~]+)\~\~/',  '<del>$1</del>', $message);
+        $message = preg_replace('/`(.+?)`/', '<code>$1</code>', $message);
+        $message = preg_replace('/(?<![\w\/])(0x[a-f0-9]{8})(?!\w)/', '<address>$1</address>', $message);
+        // легаси-разметка зачёркиваний и подчёркиваний
+        $message = preg_replace('/(?<!\S)\_(?!\s)(.+?)(?<!\s)\_(?!\S)/u',  '<u>$1</u>', $message);
         $message = preg_replace('/(?<!\S)\-(?!\s)(.+?)(?<!\s)\-(?!\S)/u',  '<del>$1</del>', $message);
-        $message = preg_replace(   '/`(.+?)`/', '<code>$1</code>', $message);
-        $message = preg_replace(   '/(?<![\w\/])(0x[a-f0-9]{8})(?!\w)/', '<address>$1</address>', $message);
 
         $message = nl2br($message);
 
