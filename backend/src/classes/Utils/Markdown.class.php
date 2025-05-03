c833dd405a700d9b21fddd797a4283c89126253c
Vlad Kortnev <194586003+katzterd@users.noreply.github.com>
2025-05-03 18:55:33 +0300

    markup and faq small fixes (#42)

--- backend/src/classes/Utils/Markdown.class.php
+++ backend/src/classes/Utils/Markdown.class.php
@@ -1,6 +1,7 @@
 <?php
 
-class Markdown {
+class Markdown
+{
     public static function format($message)
     {
         if (empty($message)) {
@@ -39,6 +40,29 @@ class Markdown {
 
         $takeoutStore = [];
 
+        // типографизация
+        $message = preg_replace('/(^|\s|\*|\^|\%|\~)--($|\s|\*|\^|\%|\~)/', '$1&mdash;$2', $message);
+        $message = preg_replace('/(^|\s|\*|\^|\%|\~)-($|\s|\*|\^|\%|\~)/', '$1&mdash;$2', $message);
+        $message = preg_replace('/(^|\s|\*|\^|\%|\~)&quot;(.+?)&quot;($|\s|\pP|\*|\^|\%|\~)/', '$1&laquo;$2&raquo;$3', $message);
+        // разметка цитат
+        $message = preg_replace('/&gt;&gt;\s*(.+?)\s*&lt;&lt;/m', '<blockquote>&quot;$1&quot;</blockquote>', $message);
+        $message = preg_replace('/^&gt;\s*(.+)$/m', '<blockquote>&gt; $1</blockquote>', $message);
+        $message = preg_replace('/^&lt;\s*(.+)$/m', '<blockquote class="revquo">&lt; $1</blockquote>', $message);
+        // остальная разметка
+        $message = preg_replace('/\(\(\(([^\(\)]+)\)\)\)/', '<span class="them">((($1)))</span>', $message);
+        $message = preg_replace('/%%([\s\S]+?)%%/', '<mark>$1</mark>', $message);
+        $message = preg_replace('/\*\*([^\*]+)\*\*/', '<b>$1</b>', $message);
+        $message = preg_replace('/\*([^\*]+)\*/',   '<i>$1</i>', $message);
+        $message = preg_replace('/\^\^([^\^]+)\^\^/', '<u>$1</u>', $message);
+        $message = preg_replace('/\~\~([^\~]+)\~\~/',  '<del>$1</del>', $message);
+        $message = preg_replace('/`(.+?)`/', '<code>$1</code>', $message);
+        $message = preg_replace('/(?<![\w\/])(0x[a-f0-9]{8})(?!\w)/', '<address>$1</address>', $message);
+        // легаси-разметка зачёркиваний и подчёркиваний
+        $message = preg_replace('/(?<!\S)\__(?!\s)(.+?)(?<!\s)\__(?!\S)/u',  '<u>$1</u>', $message);
+        $message = preg_replace('/(?<!\S)\--(?!\s)(.+?)(?<!\s)\--(?!\S)/u',  '<del>$1</del>', $message);
+
+        $message = nl2br($message);
+
         foreach ($takeouts as $takeout) {
             $message = preg_replace_callback(
                 $takeout['regexp'],
@@ -50,29 +74,6 @@ class Markdown {
                 $message
             );
         }
-
-        // типографизация
-        $message = preg_replace('/(^|\s|\*|\^|\%|\~)--($|\s|\*|\^|\%|\~)/', '$1&mdash;$2', $message);
-        $message = preg_replace('/(^|\s|\*|\^|\%|\~)-($|\s|\*|\^|\%|\~)/', '$1&mdash;$2', $message);
-        $message = preg_replace('/(^|\s|\*|\^|\%|\~)&quot;(.+?)&quot;($|\s|\pP|\*|\^|\%|\~)/', '$1&laquo;$2&raquo;$3', $message);
-        // разметка цитат
-        $message = preg_replace('/&gt;&gt;\s*(.+?)\s*&lt;&lt;/m', '<blockquote>&quot;$1&quot;</blockquote>', $message);
-        $message = preg_replace('/^&gt;\s*(.+)$/m', '<blockquote>&gt; $1</blockquote>', $message);
-        $message = preg_replace('/^&lt;\s*(.+)$/m', '<blockquote class="revquo">&lt; $1</blockquote>', $message);
-        // остальная разметка
-	$message = preg_replace('/\(\(\(([^\(\)]+)\)\)\)/', '<span class="them">((($1)))</span>', $message);
-        $message = preg_replace('/%%([\s\S]+?)%%/', '<mark>$1</mark>', $message);
-        $message = preg_replace('/\*\*([^\*]+)\*\*/', '<b>$1</b>', $message);
-        $message = preg_replace('/\*([^\*]+)\*/',   '<i>$1</i>', $message);
-        $message = preg_replace('/\^\^([^\^]+)\^\^/', '<u>$1</u>', $message);
-        $message = preg_replace('/\~\~([^\~]+)\~\~/',  '<del>$1</del>', $message);
-        $message = preg_replace('/`(.+?)`/', '<code>$1</code>', $message);
-        $message = preg_replace('/(?<![\w\/])(0x[a-f0-9]{8})(?!\w)/', '<address>$1</address>', $message);
-        // легаси-разметка зачёркиваний и подчёркиваний
-        $message = preg_replace('/(?<!\S)\_(?!\s)(.+?)(?<!\s)\_(?!\S)/u',  '<u>$1</u>', $message);
-        $message = preg_replace('/(?<!\S)\-(?!\s)(.+?)(?<!\s)\-(?!\S)/u',  '<del>$1</del>', $message);
-
-        $message = nl2br($message);
 
         foreach ($takeoutStore as $find => $replace) {
             $message = str_replace($find, $replace, $message);
