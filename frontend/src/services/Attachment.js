c3f2dc59308482c6ab238bf3b002e6ada1b96e6e
katzterd <194586003+katzterd@users.noreply.github.com>
2025-05-16 02:13:46 +0300

    format all services

--- frontend/src/services/Attachment.js
+++ frontend/src/services/Attachment.js
@@ -1,18 +1,24 @@
-import Api from './Api'
+import Api from "./Api";
 
 export default {
     upload(file, progressCallback) {
         const formData = new FormData();
-        formData.append('file', file);
-        return Api.post('attachment/upload', formData, { onUploadProgress: progressCallback });
+        formData.append("file", file);
+        return Api.post("attachment/upload", formData, {
+            onUploadProgress: progressCallback,
+        });
     },
     embed(url) {
-        return Api.post('attachment/embed', {}, { params: { url: url } });
+        return Api.post("attachment/embed", {}, { params: { url: url } });
     },
     cancel(id, token) {
-        return Api.get('attachment/cancel', { params: { attachment: id, token: token } });
+        return Api.get("attachment/cancel", {
+            params: { attachment: id, token: token },
+        });
     },
     markNsfw(id, token, isNsfw) {
-        return Api.get('attachment/markNsfw', { params: { attachment: id, token: token, isNsfw: isNsfw }});
-    }
-}
+        return Api.get("attachment/markNsfw", {
+            params: { attachment: id, token: token, isNsfw: isNsfw },
+        });
+    },
+};
--- frontend/src/services/Dialog.js
+++ frontend/src/services/Dialog.js
@@ -1,28 +1,41 @@
-import Api from './Api'
+import Api from "./Api";
 
 export default {
     list() {
-        return Api.get('dialog/list');
+        return Api.get("dialog/list");
     },
     start(to) {
-        return Api.get('dialog/start', { params: { to: to }});
+        return Api.get("dialog/start", { params: { to: to } });
     },
     get(asAddress, toAddress, after, before) {
-        return Api.get('dialog', { params: { as: asAddress, to: toAddress, after: after, before: before }});
+        return Api.get("dialog", {
+            params: {
+                as: asAddress,
+                to: toAddress,
+                after: after,
+                before: before,
+            },
+        });
     },
     send(asAddress, toAddress, message) {
-        return Api.post('dialog/send', { text: message }, { params: { as: asAddress, to: toAddress }});
+        return Api.post(
+            "dialog/send",
+            { text: message },
+            { params: { as: asAddress, to: toAddress } }
+        );
     },
     addIdentity(name) {
-        return Api.post('dialog/addIdentity', { name });
+        return Api.post("dialog/addIdentity", { name });
     },
     deleteIdentity(address) {
-        return Api.get('dialog/deleteIdentity', { params: { address: address } });
+        return Api.get("dialog/deleteIdentity", {
+            params: { address: address },
+        });
     },
     deleteDialog(dialogId) {
-        return Api.get('dialog/deleteDialog', { params: { dialog: dialogId } });
+        return Api.get("dialog/deleteDialog", { params: { dialog: dialogId } });
     },
     identities() {
-        return Api.get('dialog/identities');
-    }
-}
+        return Api.get("dialog/identities");
+    },
+};
